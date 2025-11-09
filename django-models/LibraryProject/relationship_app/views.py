f999b5b7e7c0191bf8ad214208d7e5f8183671a2
Lekia Serekara <lekiakara@gmail.com>
2025-11-09 22:43:42 +0100

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,38 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book, Library
-
-# -----------------------------
-# FUNCTION-BASED VIEW
-# -----------------------------
-def book_list_view(request):
-    """Display all books with their authors."""
-    books = Book.objects.select_related('author').all()
-
-    # Option 1: Plain text output
-    # output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    # return HttpResponse(output, content_type="text/plain")
-
-    # Option 2 (Recommended): Render HTML template
-    return render(request, "relationship_app/book_list.html", {"books": books})
+<!-- list_books.html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>List of Books</title>
+</head>
+<body>
+    <h1>Books Available:</h1>
+    <ul>
+        {% for book in books %}
+        <li>{{ book.title }} by {{ book.author.name }}</li>
+        {% endfor %}
+    </ul>
+</body>
+</html>
 
 
-
-# -----------------------------
-# CLASS-BASED VIEW
-# -----------------------------
-class LibraryDetailView(DetailView):
-    """Display details for a specific library, including all books."""
-    model = Library
-    template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
-
-    # Add books to context
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["books"] = self.object.books.all()
-        return context
+<!-- library_detail.html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Library Detail</title>
+</head>
+<body>
+    <h1>Library: {{ library.name }}</h1>
+    <h2>Books in Library:</h2>
+    <ul>
+        {% for book in library.books.all %}
+        <li>{{ book.title }} by {{ book.author.name }} (Published {{ book.publication_year }})</li>
+        {% endfor %}
+    </ul>
+</body>
+</html>
