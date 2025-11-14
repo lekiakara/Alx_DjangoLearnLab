d2595753705a0bf9e95dd6107ff95aa65ace7e32
Lekia Serekara <lekiakara@gmail.com>
2025-11-14 04:08:20 +0100

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-# Function-Based View
+from .models import Book
 
 def list_books(request):
-    """
-    A function-based view that lists all books and their authors.
-    """
-
-    books = Book.objects.select_related("author").all()
-
-    # Simple text output
-    output = "List of Books:\n\n"
-    for book in books:
-        output += f"- {book.title} by {book.author.name}\n"
-
-    return HttpResponse(output, content_type="text/plain")
+    books = Book.objects.all()   # REQUIRED
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
 
